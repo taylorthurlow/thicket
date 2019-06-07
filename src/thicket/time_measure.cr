06fdbb0239aace569c6f657ad4f4cdbcab559996
Taylor Thurlow <taylorthurlow8@gmail.com>
2019-06-07 00:06:23 -0700

    Run formatter

--- src/thicket/time_measure.cr
+++ src/thicket/time_measure.cr
@@ -7,11 +7,11 @@ module Thicket
       end
     end
 
-    YEAR = Measure.new(31_104_000, "Y", 155_520_000)
-    MONTH = Measure.new(2_592_000, "M", 15_552_000)
-    WEEK = Measure.new(604_800, "w", 2_419_200)
-    DAY = Measure.new(86_400, "d", 172_800)
-    HOUR = Measure.new(3_600, "h", 3_600)
+    YEAR   = Measure.new(31_104_000, "Y", 155_520_000)
+    MONTH  = Measure.new(2_592_000, "M", 15_552_000)
+    WEEK   = Measure.new(604_800, "w", 2_419_200)
+    DAY    = Measure.new(86_400, "d", 172_800)
+    HOUR   = Measure.new(3_600, "h", 3_600)
     MINUTE = Measure.new(60, "m", 60)
     SECOND = Measure.new(1, "s", 0)
 
