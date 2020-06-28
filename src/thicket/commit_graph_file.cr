4ff4fe394c5bf047cef155cf3cd263eeebd4faad
Taylor Thurlow <taylorthurlow@me.com>
2020-06-28 12:19:44 -0700

    Start implementing commit data chunk parsing

--- src/thicket/commit_graph_file.cr
+++ src/thicket/commit_graph_file.cr
@@ -11,6 +11,7 @@ module Thicket
     getter oid_fanout : Array(UInt32)
     getter num_commits : UInt32
     getter commit_oids : Array(String)
+    getter commit_data : Array(CommitData)
     
     def initialize(@file_path)
       file = File.new(@file_path, "rb")
@@ -32,6 +33,9 @@ module Thicket
       @commit_oids = parse_oid_lookup(file, contents)
       puts "First commit: #{@commit_oids.first[0..6]}"
       puts " Last commit: #{@commit_oids.last[0..6]}"
+
+      @commit_data = parse_commit_data(file, contents, @num_commits)
+      pp @commit_data.first
 
       file.close
     end
@@ -140,6 +144,61 @@ module Thicket
       end
 
       oids.reverse!
+    end
+    
+    private def parse_commit_data(
+      file : File,
+      contents : Array({ signature: String, offset: UInt64 }),
+      num_commits : UInt32,
+    ) : Array(CommitData)
+      commit_data_index = contents.index { |c| c[:signature] == "CDAT" }.not_nil!
+      commit_data_offset = contents[commit_data_index][:offset]
+
+      commit_data_length = if contents[commit_data_index + 1]?
+        contents[commit_data_index + 1][:offset] - commit_data_offset
+      else
+        # Exclude trailer hash if necessary
+        file.size - commit_hash_length - commit_data_offset
+      end
+
+      single_commit_data_size = commit_hash_length + 16
+      slice = Bytes.new(@num_commits * single_commit_data_size)
+      file.read_at(commit_data_offset.to_i32, commit_data_length.to_i32, &.read(slice))
+      slice.reverse!
+
+      puts "Found #{slice.size} bytes of commit data."
+
+      Array.new(num_commits) do |i|
+        subslice = slice[i, single_commit_data_size]
+
+        root_tree_oid = subslice[0, commit_hash_length].to_a
+                                                       .map { |b| sprintf("%02x", b) }
+                                                       .reverse
+                                                       .join
+
+        first_parent_slice = subslice[commit_hash_length, 4]
+        first_parent_value = first_parent_slice.to_unsafe.as(UInt32*).value
+        first_parent = first_parent_value == 0x7000000 ? nil : first_parent_value
+
+        second_parent_slice = subslice[commit_hash_length, 4]
+        second_parent_value = second_parent_slice.to_unsafe.as(UInt32*).value
+        second_parent = second_parent_value == 0x7000000 ? nil : second_parent_value
+
+        CommitData.new(
+          root_tree_oid,
+          first_parent,
+          second_parent,
+        )
+      end
+    end
+
+    struct CommitData
+      getter root_tree_oid : String
+      getter first_parent : UInt32 | Nil
+      getter second_parent : UInt32 | Nil
+      
+      def initialize(@root_tree_oid, @first_parent, @second_parent)
+      end
     end
   end
 end
